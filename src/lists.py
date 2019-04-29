c40593d415e58e32781fa1653e2838b27c21aac7
Jacob Bryan <jtb36688@gmail.com>
2019-04-29 19:07:49 -0400

    lists completed showing all python list methods

--- src/lists.py
+++ src/lists.py
@@ -7,23 +7,21 @@ y = [8, 9, 10]
 # For the following, DO NOT USE AN ASSIGNMENT (=).
 
 # Change x so that it is [1, 2, 3, 4]
-# YOUR CODE HERE 
-print(x)
+x.append(4)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
-# YOUR CODE HERE 
-print(x)
+x.extend(y)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
-# YOUR CODE HERE 
-print(x)
+x.remove(8)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
-# YOUR CODE HERE 
+x.insert(-1, 99)
 print(x)
 
 # Print the length of list x
-# YOUR CODE HERE 
+print(len(x))
 
 # Print all the values in x multiplied by 1000
-# YOUR CODE HERE
+for num in x: 
+    print(num*1000)
