1255b5c7e52f4c373095c7f080e123f3ab16db33
Jacob Bryan <jtb36688@gmail.com>
2019-04-29 20:55:21 -0400

    Finished comprehensions

--- src/comprehensions.py
+++ src/comprehensions.py
@@ -10,14 +10,14 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+y = [y+1 for y in range(5)]
 
 print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+y = [(y+1)**3 for y in range(9)]
 
 print(y)
 
@@ -26,16 +26,16 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [s.upper() for s in a]
 
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
 
-x = input("Enter comma-separated numbers: ").split(',')
+x = input("Enter Numbers:").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [s for s in x if int(s) % 2 == 0]
 
 print(y)
